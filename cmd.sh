a57de7fa50c0288cf2305082b49134c3d7557f65
Adarsh Kumar <58614055+i-adarsh@users.noreply.github.com>
2025-10-07 23:23:29 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,3 +5,17 @@ sudo apt update -y
 sudo apt-get install xdg-utils snapd -y
 
 sudo snap install ngrok -y
+
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
+
+  ngrok config add-authtoken 33kPvkR2haJGTQETjWX8Ls5Ln3N_47qJ47WDZo4fvdYJTS7As
+
+  ngrok http 3000
+
+
+  shopify app dev --tunnel-url=https://<ngrok-URL>:3000
